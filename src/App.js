697f56d8f18ebcbffdb9ec88a29ce22ea568def2
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-07-26 19:13:09 +0500

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import CategoryTable from "./components/CategoryTable";
 import Sidebar from "./components/SideBar";
 import Dashboard from "./components/Dashboard";
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       {/* <Sidebar/> */}
+      <Navbar/>
       <Dashboard/>
         <CategoryTable/>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+// src/Navbar.js
+import React from 'react';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="search-container">
+                <input type="text" placeholder="Search here..." />
+                <button type="button"><i className="fa fa-search"></i></button>
+            </div>
+            <div className="icons-container">
+                <div className="icon"><i className="fa fa-moon"></i></div>
+                <div className="icon notification"><i className="fa fa-bell"></i><span className="badge">1</span></div>
+                <div className="icon message"><i className="fa fa-comment"></i><span className="badge">1</span></div>
+                <div className="icon"><i className="fa fa-expand"></i></div>
+                <div className="icon"><i className="fa fa-th"></i></div>
+                <div className="profile">
+                    <img src="https://via.placeholder.com/40" alt="Profile" />
+                    <span>Kristin Watson</span>
+                    <span className="role">Admin</span>
+                </div>
+                <div className="icon"><i className="fa fa-cog"></i></div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
